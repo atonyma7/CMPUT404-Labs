3554e804093c4334351b7559848461e5f549b992
atonyma7 <atonyma7@gmail.com>
2021-09-08 18:40:27 -0600

    using requests.text

--- lab_1.py
+++ lab_1.py
@@ -1,4 +1,5 @@
 import requests
 print (requests.__version__)
 requests.get('http://www.google.com')
-print(requests.get('https://raw.githubusercontent.com/atonyma7/CMPUT404-Labs/main/lab_1.py').request.body);
+my_request = requests.get('https://raw.githubusercontent.com/atonyma7/CMPUT404-Labs/main/lab_1.py')
+print(my_request.text);
